0c94731d4664206ee4d231af62ef8d85b8b3adb7
Vadym <minerking112256@gmail.com>
2026-07-13 18:38:58 +0200

    Create Size.cs

---
+++ backend/SneakersShop.Domain/Warehouse/ValueObjects/Size.cs
@@ -0,0 +1,16 @@
+﻿using SneakersShop.Domain.Common.Exceptions;
+
+namespace SneakersShop.Domain.Warehouse.ValueObjects;
+
+public sealed record Size
+{
+    public decimal ValueCm { get; }
+
+    public Size(decimal valueCm)
+    {
+        if (valueCm <= 0)
+            throw new DomainException(nameof(valueCm), "Size must be greater than zero.");
+        ValueCm = valueCm;
+    }
+    private Size() { }
+}
