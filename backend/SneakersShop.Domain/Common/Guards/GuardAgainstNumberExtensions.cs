cb374922e03f1ce491835b75f10059b36c96158d
Vadym <minerking112256@gmail.com>
2026-07-14 20:42:33 +0200

    Update GuardAgainstNumberExtensions.cs
    Add new against extensions: Negative

--- backend/SneakersShop.Domain/Common/Guards/GuardAgainstNumberExtensions.cs
+++ backend/SneakersShop.Domain/Common/Guards/GuardAgainstNumberExtensions.cs
@@ -3,6 +3,16 @@
 namespace SneakersShop.Domain.Common.Guards;
 public static class GuardAgainstNumberExtensions
 {
+    public static int Negative(
+        this IGuard guard,
+        int value,
+        [CallerArgumentExpression(nameof(value))] string? parameterName = null)
+    {
+        if (value < 0)
+            throw new ArgumentException($"Parameter '{parameterName}' must be non-negative.", parameterName);
+        return value;
+    }
+
     public static int NegativeOrZero(
         this IGuard guard,
         int value,
