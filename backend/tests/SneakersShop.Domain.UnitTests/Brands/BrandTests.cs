1665aaa50e26f568c2556b4003efc3f115be27ec
Vadym <minerking112256@gmail.com>
2026-07-19 17:16:59 +0200

    Create BrandTests.cs

---
+++ backend/tests/SneakersShop.Domain.UnitTests/Brands/BrandTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+
+using SneakersShop.Domain.Brands;
+
+namespace SneakersShop.Domain.UnitTests.Brands;
+
+public class BrandTests
+{
+    [Fact]
+    public void Create_WithValidName_ReturnsBrand()
+    {
+        var brand = Brand.Create("Nike");
+
+        brand.Name.Should().Be("Nike");
+        brand.Id.Should().NotBe(Guid.Empty);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Create_WithBlankName_Throws(string name)
+    {
+        var act = () => Brand.Create(name);
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void UpdateName_ChangesName()
+    {
+        var brand = Brand.Create("Nike");
+
+        brand.UpdateName("Adidas");
+
+        brand.Name.Should().Be("Adidas");
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void UpdateName_WithBlankName_Throws(string name)
+    {
+        var brand = Brand.Create("Nike");
+
+        var act = () => brand.UpdateName(name);
+        act.Should().Throw<ArgumentException>();
+    }
+}
