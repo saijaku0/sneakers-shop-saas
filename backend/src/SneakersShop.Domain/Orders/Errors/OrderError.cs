853e65f04fc3acc5a925cf436fd0390d1434fad5
Vadym <minerking112256@gmail.com>
2026-07-19 12:00:59 +0200

    Create OrderError.cs

---
+++ backend/src/SneakersShop.Domain/Orders/Errors/OrderError.cs
@@ -0,0 +1,12 @@
+﻿using SneakersShop.Domain.Common.Results;
+using SneakersShop.Domain.Orders.Enums;
+
+namespace SneakersShop.Domain.Orders.Errors;
+
+public static class OrderError
+{
+    public static Error InvalidStateTransition(OrderStatus currentStatus, OrderStatus attemptedStatus) =>
+        Error.Invalid("Order.InvalidStateTransition", $"Cannot transition order from {currentStatus} to {attemptedStatus}.");
+    public static Error PaymentDeadlineExpired(DateTimeOffset paymentDeadline) =>
+        Error.Invalid("Order.PaymentDeadlineExpired", $"Payment deadline of {paymentDeadline} has expired.");
+}
