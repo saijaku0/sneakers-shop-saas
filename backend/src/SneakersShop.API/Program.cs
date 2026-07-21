d5c54aa3805f121c14b56a216405328349d7f317
Vadym <minerking112256@gmail.com>
2026-07-21 17:58:51 +0200

    Update Program.cs
    Connect infrastructure factory

--- backend/src/SneakersShop.API/Program.cs
+++ backend/src/SneakersShop.API/Program.cs
@@ -1,6 +1,9 @@
+using SneakersShop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
