2bb56fbe8b89fd5a6c8ec29edd100ea7a0cbd0cc
Vadym <minerking112256@gmail.com>
2026-07-22 18:32:42 +0200

    Create BrandConfiguration.cs

---
+++ backend/src/SneakersShop.Infrastructure/Persistence/Configurations/BrandConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using SneakersShop.Domain.Brands;
+
+namespace SneakersShop.Infrastructure.Persistence.Configurations;
+
+internal sealed class BrandConfiguration : IEntityTypeConfiguration<Brand>
+{
+    public void Configure(EntityTypeBuilder<Brand> builder)
+    {
+        builder.ToTable($"{nameof(Brand)}s");
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.Name).IsRequired();
+    }
+}
