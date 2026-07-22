69e1f2ee72fdcf6e8a0fb9ad4ce80cf0354c3bd5
Vadym <minerking112256@gmail.com>
2026-07-22 18:32:44 +0200

    Create CartConfiguration.cs

---
+++ backend/src/SneakersShop.Infrastructure/Persistence/Configurations/CartConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using SneakersShop.Domain.Carts;
+
+namespace SneakersShop.Infrastructure.Persistence.Configurations;
+
+internal sealed class CartConfiguration : IEntityTypeConfiguration<Cart>
+{
+    public void Configure(EntityTypeBuilder<Cart> builder)
+    {
+        builder.ToTable($"{nameof(Cart)}s");
+        builder.HasKey(c => c.Id);
+        builder.HasMany(c => c.Items)
+            .WithOne()
+            .HasForeignKey("CartId")
+            .OnDelete(DeleteBehavior.Cascade);
+        builder.Navigation(c => c.Items)
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
+        builder.Property(c => c.UserId).IsRequired();
+        builder.HasIndex(c => c.UserId).IsUnique();
+    }
+}
