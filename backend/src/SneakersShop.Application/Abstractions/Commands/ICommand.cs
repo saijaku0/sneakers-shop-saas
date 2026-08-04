6f7a72677737034f85bb254ceed45d2ba4472f6c
Vadym <minerking112256@gmail.com>
2026-08-04 16:45:43 +0200

    Create ICommand.cs

---
+++ backend/src/SneakersShop.Application/Abstractions/Commands/ICommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+using SneakersShop.Domain.Common.Results;
+
+namespace SneakersShop.Application.Abstractions.Commands;
+
+public interface ICommand : IRequest<Result>;
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>;
