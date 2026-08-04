785e50c899e42411cc8021be18c60e0cea9e4816
Vadym <minerking112256@gmail.com>
2026-08-04 16:48:29 +0200

    Create WarehouseItemRepository.cs

---
+++ backend/src/SneakersShop.Infrastructure/Persistence/Repositories/WarehouseItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using SneakersShop.Application.Abstractions.Repositories;
+using SneakersShop.Domain.Warehouse;
+
+namespace SneakersShop.Infrastructure.Persistence.Repositories;
+
+internal sealed class WarehouseItemRepository(AppDbContext appDbContext) : IWarehouseItemRepository
+{
+    public async Task<WarehouseItem?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+        await appDbContext.WarehouseItems.FindAsync([id], cancellationToken);
+}
