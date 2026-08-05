72ce2587bd0c092067a010ee7d69f5470c457c13
Vadym <minerking112256@gmail.com>
2026-08-05 16:51:05 +0200

    Create TestServiceProviderFactory.cs

---
+++ backend/tests/SneakersShop.IntegrationTests/Infrastructure/TestServiceProviderFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using SneakersShop.Application;
+using SneakersShop.Infrastructure;
+
+namespace SneakersShop.IntegrationTests.Infrastructure;
+
+public static class TestServiceProviderFactory
+{
+    public static IServiceProvider Build(string connectionString)
+    {
+        ServiceCollection services = new();
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                { "ConnectionStrings:DefaultConnection", connectionString }
+            })
+            .Build();
+
+        services.AddSingleton<IConfiguration>(configuration);
+
+        services.AddApplication();
+        services.AddInfrastructure(configuration);
+
+        return services.BuildServiceProvider();
+    }
+}
