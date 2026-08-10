9c15afab831f2fff258ea99abcb4385f5dcf7c45
Vadym <minerking112256@gmail.com>
2026-08-10 21:12:11 +0200

    Create JwtSettings.cs

---
+++ backend/src/SneakersShop.Infrastructure/Persistence/Auth/JwtSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace SneakersShop.Infrastructure.Persistence.Auth;
+
+/// <summary>
+/// Represents the settings for JWT (JSON Web Token) authentication.
+/// </summary>
+internal class JwtSettings
+{
+    public string SecretKey { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public int AccessTokenExpiryMinutes { get; set; }
+    public int RefreshTokenExpiryDays { get; set; }
+}
