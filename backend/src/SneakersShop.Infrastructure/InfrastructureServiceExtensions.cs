e091b2d84209ac5322574cc4f70035b452577466
Vadym <minerking112256@gmail.com>
2026-08-10 21:12:00 +0200

    Register JWTSetting config and Auth scoped

--- backend/src/SneakersShop.Infrastructure/InfrastructureServiceExtensions.cs
+++ backend/src/SneakersShop.Infrastructure/InfrastructureServiceExtensions.cs
@@ -2,8 +2,10 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using SneakersShop.Application.Abstractions.Authentication;
 using SneakersShop.Application.Abstractions.Repositories;
 using SneakersShop.Infrastructure.Persistence;
+using SneakersShop.Infrastructure.Persistence.Auth;
 using SneakersShop.Infrastructure.Persistence.Repositories;
 
 namespace SneakersShop.Infrastructure;
@@ -22,6 +24,10 @@ public static class InfrastructureServiceExtensions
         // Register repositories and unit of work
         services.AddScoped<IWarehouseItemRepository, WarehouseItemRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        //Register jwt settings and service
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
 
         return services;
     }
