3529c4696d2956ecbce125729706c18cbc467d83
Vadym <minerking112256@gmail.com>
2026-08-10 21:11:13 +0200

    Create TokenGenerationRequest.cs

---
+++ backend/src/SneakersShop.Application/Auth/DTOs/TokenGenerationRequest.cs
@@ -0,0 +1,9 @@
+﻿using SneakersShop.Domain.Consumer.Enums;
+
+namespace SneakersShop.Application.Auth.DTOs;
+
+public record TokenGenerationRequest(
+    Guid UserId,
+    string Email,
+    IEnumerable<UserRoles> Roles
+);
