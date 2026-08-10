4142dbe4fa0efc517ac8b4406ee2ed9a58e72db4
Vadym <minerking112256@gmail.com>
2026-08-10 21:12:13 +0200

    Create UserProfileConfiguration.cs

---
+++ backend/src/SneakersShop.Infrastructure/Persistence/Configurations/UserProfileConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using SneakersShop.Domain.Consumer;
+
+namespace SneakersShop.Infrastructure.Persistence.Configurations;
+
+internal sealed class UserProfileConfiguration : IEntityTypeConfiguration<UserProfile>
+{
+    public void Configure(EntityTypeBuilder<UserProfile> builder)
+    {
+        builder.ToTable($"{nameof(UserProfile)}s");
+        builder.HasKey(up => up.Id);
+        builder.OwnsOne(up => up.DefaultAddress, address =>
+        {
+            address.Property(a => a.Street)
+                .HasMaxLength(200)
+                .IsRequired(false);
+            address.Property(a => a.City)
+                .HasMaxLength(100)
+                .IsRequired(false);
+            address.Property(a => a.State)
+                .HasMaxLength(100)
+                .IsRequired(false);
+            address.Property(a => a.HouseNumber)
+                .HasMaxLength(100)
+                .IsRequired(false);
+            address.Property(a => a.ZipCode)
+                .HasMaxLength(20)
+                .IsRequired(false);
+            address.Property(a => a.Country)
+                .HasMaxLength(100)
+                .IsRequired(false);
+        });
+        builder.Property(up => up.IsFlagged)
+            .IsRequired();
+    }
+}
