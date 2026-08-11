61002788f6536b941598a94efd022388c0cd7b48
Vadym <minerking112256@gmail.com>
2026-08-11 14:52:32 +0200

    Create RegisterNewUserHandler.cs

---
+++ backend/src/SneakersShop.Application/Auth/Commands/RegisterNewUser/RegisterNewUserHandler.cs
@@ -0,0 +1,24 @@
+﻿using SneakersShop.Application.Abstractions.Authentication;
+using SneakersShop.Application.Abstractions.Commands;
+using SneakersShop.Application.Abstractions.Repositories;
+using SneakersShop.Application.Auth.DTOs;
+using SneakersShop.Domain.Common.Results;
+
+namespace SneakersShop.Application.Auth.Commands.RegisterNewUser;
+
+public sealed class RegisterNewUserHandler(
+    IAuthenticationService authService,
+    IUnitOfWork unitOfWork)
+    : CommandHandler<RegisterCommand, AuthResponse>(unitOfWork)
+{
+    protected override async Task<Result<AuthResponse>> HandleCommandAsync(
+        RegisterCommand request,
+        CancellationToken cancellationToken)
+    {
+        var createUser = await authService.CreateUserAsync(request, cancellationToken).ConfigureAwait(false);
+        if (createUser == null)
+            return Result<AuthResponse>.Failure(new Error("Auth.InvalidCredentials", "User creation failed.", ErrorType.Invalid));
+
+        return await authService.LoginAsync(request.Email, request.Password, cancellationToken);
+    }
+}
