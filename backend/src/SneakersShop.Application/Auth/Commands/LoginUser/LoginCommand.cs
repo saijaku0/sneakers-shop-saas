cbb35bc3078f458848bdb745d7dd5a0f4b9a0fb0
Vadym <minerking112256@gmail.com>
2026-08-11 14:52:18 +0200

    Create LoginCommand.cs

---
+++ backend/src/SneakersShop.Application/Auth/Commands/LoginUser/LoginCommand.cs
@@ -0,0 +1,6 @@
+﻿using SneakersShop.Application.Abstractions.Commands;
+using SneakersShop.Application.Auth.DTOs;
+
+namespace SneakersShop.Application.Auth.Commands.LoginUser;
+
+public record LoginCommand(string Email, string Password) : ICommand<AuthResponse>;
