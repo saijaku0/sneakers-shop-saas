cc41654094e74d6a1069cfceb5e7a115c51983a3
Vadym <minerking112256@gmail.com>
2026-08-13 15:02:35 +0200

    Create AuthenticationFlowTests.cs

---
+++ backend/tests/SneakersShop.IntegrationTests/Authorization/AuthenticationFlowTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+using FluentAssertions;
+
+using SneakersShop.IntegrationTests.Infrastructure;
+
+namespace SneakersShop.IntegrationTests.Authorization;
+
+public class AuthenticationFlowTests : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    private readonly TestAuthHelper _authHelper;
+
+    public AuthenticationFlowTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+        _authHelper = new TestAuthHelper(_client);
+    }
+
+    [Fact]
+    public async Task CompleteFlow_RegisterLoginAndAccessProtectedResource_Succeeds()
+    {
+        var registerResponse = await _authHelper.RegisterUserAsync();
+        registerResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        var loginResponse = await _authHelper.LoginUserAsync();
+        loginResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        var loginContent = await loginResponse.Content.ReadAsStringAsync();
+        using var document = JsonDocument.Parse(loginContent);
+        var token = document.RootElement.GetProperty("accessToken").GetString();
+
+        var fakeId = Guid.NewGuid();
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/api/v1/warehouseitems/{fakeId}/reserve");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var finalResponse = await _client.SendAsync(request);
+
+        finalResponse.StatusCode.Should().NotBe(System.Net.HttpStatusCode.Unauthorized);
+    }
+}
