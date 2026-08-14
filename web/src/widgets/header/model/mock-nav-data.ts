7f75fb5072a909b7ca1937dc0e5565c4a24b2e41
Vadym <minerking112256@gmail.com>
2026-08-14 17:24:37 +0200

    Create mock-nav-data.ts

---
+++ web/src/widgets/header/model/mock-nav-data.ts
@@ -0,0 +1,14 @@
+export const mockNavData = {
+  categories: {
+    men: ["Sneakers", "Running", "Boots", "Sandals", "Shoes"],
+    women: ["Sneakers", "Running", "Boots", "Heels", "Sandals"],
+    kids: ["Sneakers", "Running", "Boots"],
+  },
+  brands: [
+    { name: "Nike", slug: "nike" },
+    { name: "Adidas", slug: "adidas" },
+    { name: "New Balance", slug: "new-balance" },
+    { name: "Puma", slug: "puma" },
+    { name: "Reebok", slug: "reebok" },
+  ],
+};
