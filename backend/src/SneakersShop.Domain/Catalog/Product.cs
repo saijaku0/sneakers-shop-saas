2bd2905b56101499ead0bceba3540e4b8d4413c0
Vadym <minerking112256@gmail.com>
2026-08-15 13:28:33 +0200

    Update Product.cs

--- backend/src/SneakersShop.Domain/Catalog/Product.cs
+++ backend/src/SneakersShop.Domain/Catalog/Product.cs
@@ -1,59 +1,58 @@
-﻿using SneakersShop.Domain.Catalog.DomainEvents;
-using SneakersShop.Domain.Catalog.ValueObjects;
+﻿using System.Reflection;
+
+using SneakersShop.Domain.Catalog.DomainEvents;
+using SneakersShop.Domain.Catalog.Enums;
 using SneakersShop.Domain.Common.Entities;
 using SneakersShop.Domain.Common.Guards;
 
 namespace SneakersShop.Domain.Catalog;
 
-/* TODO: Update model 
- * add property: Color;
- * 
- * Need think about price is decimal or Money value object;
- * 
- * braking changes: Color, need update UML view and database schema
-*/
-
 public sealed class Product : AggregateRoot
 {
     public Guid BrandId { get; private init; }
+    public Guid CategoryId { get; private init; }
+    public Gender Gender { get; private set; }
     public string Model { get; private set; } = null!;
     public string Description { get; private set; } = null!;
-    public decimal Price { get; private set; }
+    public decimal BasePrice { get; private set; }
     public bool IsActive { get; private set; }
-    public IReadOnlyCollection<ProductImage> Images => _images.AsReadOnly();
-    private readonly List<ProductImage> _images = [];
 
     private Product() { }
+
     private Product(
+        Guid id,
         Guid brandId,
+        Guid categoryId,
+        Gender gender,
         string model,
         string description,
-        decimal price,
-        IEnumerable<ProductImage> images) : base(Guid.CreateVersion7())
+        decimal basePrice)
+        : base(id)
     {
         BrandId = brandId;
+        CategoryId = categoryId;
+        Gender = gender;
         Model = model;
         Description = description;
-        Price = price;
-        _images.AddRange(images);
-
+        BasePrice = basePrice;
         IsActive = true;
     }
 
     public static Product Create(
-    Guid brandId,
-    string model,
-    string description,
-    decimal price,
-    IEnumerable<ProductImage> images)
+        Guid brandId,
+        Guid categoryId,
+        Gender gender,
+        string model,
+        string description,
+        decimal basePrice)
     {
         Guard.Against.Empty(brandId);
+        Guard.Against.Empty(categoryId);
         Guard.Against.NullOrWhiteSpace(model);
         Guard.Against.NullOrWhiteSpace(description);
-        Guard.Against.NegativeOrZero(price);
-        Guard.Against.Null(images);
+        Guard.Against.NegativeOrZero(basePrice);
 
-        return new Product(brandId, model, description, price, images);
+        return new Product(Guid.CreateVersion7(), brandId, categoryId, gender, model, description, basePrice);
     }
 
     public void Activate()
