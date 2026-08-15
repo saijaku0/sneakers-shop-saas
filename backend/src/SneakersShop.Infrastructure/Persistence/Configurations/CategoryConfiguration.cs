c027794895958b965bd844a20ffcbdd722d342af
Vadym <minerking112256@gmail.com>
2026-08-15 13:28:43 +0200

    Create CategoryConfiguration.cs

---
+++ backend/src/SneakersShop.Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using SneakersShop.Domain.Catalog;
+
+namespace SneakersShop.Infrastructure.Persistence.Configurations;
+
+internal sealed class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+    }
+}
