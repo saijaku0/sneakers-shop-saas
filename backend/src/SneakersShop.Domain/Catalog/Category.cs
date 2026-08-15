a4ec3797a59d85ced43d6c769a1ab1acebe356f2
Vadym <minerking112256@gmail.com>
2026-08-15 13:28:28 +0200

    Create Category.cs

---
+++ backend/src/SneakersShop.Domain/Catalog/Category.cs
@@ -0,0 +1,29 @@
+﻿using SneakersShop.Domain.Common.Entities;
+using SneakersShop.Domain.Common.Guards;
+
+namespace SneakersShop.Domain.Catalog;
+
+public sealed class Category : AggregateRoot
+{
+    public string Name { get; private set; } = null!;
+
+    private Category() { }
+
+    private Category(Guid id, string name) : base(id)
+    {
+        Name = name;
+    }
+
+    public static Category Create(string name)
+    {
+        Guard.Against.NullOrWhiteSpace(name);
+        return new Category(Guid.CreateVersion7(), name);
+    }
+
+    public void UpdateName(string name)
+    {
+        Guard.Against.NullOrWhiteSpace(name);
+        Name = name;
+        Touch();
+    }
+}
