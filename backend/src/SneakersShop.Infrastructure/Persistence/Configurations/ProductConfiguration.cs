965d4b8736fa18d1c841253db9c1f7a375caea3e
Vadym <minerking112256@gmail.com>
2026-08-15 13:28:45 +0200

    Update ProductConfiguration.cs

--- backend/src/SneakersShop.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
+++ backend/src/SneakersShop.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -14,14 +14,19 @@ public sealed class ProductConfiguration : IEntityTypeConfiguration<Product>
         builder.HasKey(p => p.Id);
         builder.Property(p => p.Model).IsRequired().HasMaxLength(200);
         builder.Property(p => p.Description).IsRequired().HasMaxLength(2000);
-        builder.Property(p => p.Price).HasPrecision(18, 2);
         builder.Property(p => p.IsActive).IsRequired();
-        builder.OwnsMany(p => p.Images, img =>
-        {
-            img.ToJson();
-            img.Property(i => i.Url);
-        });
-        builder.Navigation(p => p.Images).UsePropertyAccessMode(PropertyAccessMode.Field);
+        builder.Property(p => p.Gender)
+            .HasConversion<string>()
+            .HasMaxLength(20)
+            .IsRequired();
+
+        builder.HasOne<Category>()
+            .WithMany()
+            .HasForeignKey(p => p.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Property(p => p.BasePrice)
+            .HasPrecision(18, 2);
 
         builder.HasOne<Brand>()
             .WithMany()
