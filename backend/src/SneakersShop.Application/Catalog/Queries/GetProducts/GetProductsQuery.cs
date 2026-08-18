f6b3260cb64e10406990a3566a3061cb0d62461a
Vadym <minerking112256@gmail.com>
2026-08-18 11:21:39 +0200

    Create GetProductsQuery.cs

---
+++ backend/src/SneakersShop.Application/Catalog/Queries/GetProducts/GetProductsQuery.cs
@@ -0,0 +1,20 @@
+﻿using SneakersShop.Application.Abstractions.Queries;
+using SneakersShop.Application.Catalog.DTOs;
+using SneakersShop.Application.Common.PageResults;
+using SneakersShop.Domain.Catalog.Enums;
+
+namespace SneakersShop.Application.Catalog.Queries.GetProducts;
+
+public record GetProductsQuery(
+    Gender? Gender,
+    string? Category,
+    string? Brand,
+    decimal? MinPrice = null,
+    decimal? MaxPrice = null,
+    string? Color = null,
+    decimal? Size = null,
+    bool? InStockOnly = null,
+    string? SortBy = null, // "price_asc" | "price_desc" | "newest" | "name"
+    int PageNumber = 1,
+    int PageSize = 20)
+    : IQuery<PageResult<ProductListItem>>;
