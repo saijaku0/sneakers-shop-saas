6c40fe281a473829731c193409eb701c926643d0
Vadym <minerking112256@gmail.com>
2026-08-18 11:21:32 +0200

    Create ProductsController.cs

---
+++ backend/src/SneakersShop.API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using SneakersShop.Application.Catalog.Queries.GetProducts;
+
+namespace SneakersShop.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[AllowAnonymous]
+public sealed class ProductsController(ISender sender) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetProducts(
+        [FromQuery] GetProductsQuery query,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await sender.Send(query, cancellationToken);
+        return Ok(result);
+    }
+}
