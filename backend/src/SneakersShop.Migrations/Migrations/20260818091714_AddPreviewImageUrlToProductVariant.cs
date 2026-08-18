bd43e0f9c80e7447d530c2ab8ac1540c8e36ba93
Vadym <minerking112256@gmail.com>
2026-08-18 11:22:06 +0200

    Create 20260818091714_AddPreviewImageUrlToProductVariant.cs

---
+++ backend/src/SneakersShop.Migrations/Migrations/20260818091714_AddPreviewImageUrlToProductVariant.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SneakersShop.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPreviewImageUrlToProductVariant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PreviewImageUrl",
+                table: "ProductVariants",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreviewImageUrl",
+                table: "ProductVariants");
+        }
+    }
+}
