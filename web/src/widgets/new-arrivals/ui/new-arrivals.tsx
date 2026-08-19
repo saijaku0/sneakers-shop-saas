d061b37795dd248e2d53e7626703198bc73d8d34
Vadym <minerking112256@gmail.com>
2026-08-19 10:59:35 +0200

    Create new-arrivals.tsx

---
+++ web/src/widgets/new-arrivals/ui/new-arrivals.tsx
@@ -0,0 +1,14 @@
+export function NewArrivial() {
+  return (
+    <>
+      <div className="flex flex-col gap-8 my-14">
+        <span className="uppercase md:font-bold text-sm text-foreground/70">
+          just dropped
+        </span>
+        <h1 className="uppercase font-black md:text-6xl text-4xl font-stretch-extra-expanded tracking-tight md:scale-y-150 scale-y-125">
+          new arrivals
+        </h1>
+      </div>
+    </>
+  );
+}
