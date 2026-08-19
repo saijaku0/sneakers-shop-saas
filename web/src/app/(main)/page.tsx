6c1265f4085a7938d9bb827abc080c579a37e55e
Vadym <minerking112256@gmail.com>
2026-08-19 10:58:51 +0200

    Update page.tsx

--- web/src/app/(main)/page.tsx
+++ web/src/app/(main)/page.tsx
@@ -1,3 +1,22 @@
+import { Container } from "@/shared/ui";
+import { BrandMarquee } from "@/widgets/brand-marquee";
+import { HeroCarousel } from "@/widgets/hero-carousel";
+import { NewArrivial } from "@/widgets/new-arrivals";
+
 export default function HomePage() {
-  return <section>Homepage</section>;
+  return (
+    <>
+      <section id="carousel">
+        <HeroCarousel />
+      </section>
+      <section id="marquee">
+        <BrandMarquee />
+      </section>
+      <Container>
+        <section>
+          <NewArrivial />
+        </section>
+      </Container>
+    </>
+  );
 }
