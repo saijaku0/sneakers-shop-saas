ceab31626e12dd559a8c02e1095c4b9c44084e84
Vadym <minerking112256@gmail.com>
2026-08-19 15:09:49 +0200

    Update ProductsController.cs

--- backend/src/SneakersShop.API/Controllers/ProductsController.cs
+++ backend/src/SneakersShop.API/Controllers/ProductsController.cs
@@ -3,6 +3,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+using SneakersShop.API.Extensions;
+using SneakersShop.Application.Catalog.Queries.GetProductItem;
 using SneakersShop.Application.Catalog.Queries.GetProducts;
 
 namespace SneakersShop.API.Controllers;
@@ -18,6 +20,16 @@ public sealed class ProductsController(ISender sender) : ControllerBase
         CancellationToken cancellationToken = default)
     {
         var result = await sender.Send(query, cancellationToken);
-        return Ok(result);
+        return result.ToActionResult();
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetProductItem(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        var query = new GetProductItemQuery(id);
+        var result = await sender.Send(query, cancellationToken);
+        return result.ToActionResult();
     }
 }
