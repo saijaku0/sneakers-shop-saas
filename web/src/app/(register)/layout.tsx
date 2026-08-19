c9ccb989219c4c4e241c02dceef4ecb6dadcf3d6
Vadym <minerking112256@gmail.com>
2026-08-19 10:58:53 +0200

    Create layout.tsx

---
+++ web/src/app/(register)/layout.tsx
@@ -0,0 +1,12 @@
+export default function SignUpLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div>1011</div>
+      <main>{children}</main>
+    </>
+  );
+}
