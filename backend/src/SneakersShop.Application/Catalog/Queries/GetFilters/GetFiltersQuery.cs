d7436098c7286c1be151cf5f25ac45455a2f5757
Vadym <minerking112256@gmail.com>
2026-08-21 16:36:05 +0200

    Create GetFiltersQuery.cs

---
+++ backend/src/SneakersShop.Application/Catalog/Queries/GetFilters/GetFiltersQuery.cs
@@ -0,0 +1,6 @@
+﻿using SneakersShop.Application.Abstractions.Queries;
+using SneakersShop.Application.Catalog.DTOs;
+
+namespace SneakersShop.Application.Catalog.Queries.GetFilters;
+
+public record GetFiltersQuery() : IQuery<FiltersDto>;
