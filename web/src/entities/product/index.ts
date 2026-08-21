1c99ea5a809cf5046aabb79183d9c93cc653c138
Vadym <minerking112256@gmail.com>
2026-08-21 16:35:35 +0200

    Update index.ts

--- web/src/entities/product/index.ts
+++ web/src/entities/product/index.ts
@@ -1,1 +1,14 @@
 export { ProductCard } from "./ui/product-card";
+export { buildApiParams } from "./lib/build-api-params";
+export {
+  useGetProductsQuery,
+  useGetProductByIdQuery,
+  useGetFiltersQuery,
+} from "./api/products-api";
+export {
+  type CatalogFilters,
+  type ProductDetailVariant,
+  type ProductSize,
+  type ProductDetail,
+  type ProductFilters,
+} from "./model/types";
