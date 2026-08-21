9a335fded1e91544816c2aa97b475097aa4ed750
Vadym <minerking112256@gmail.com>
2026-08-21 16:36:11 +0200

    Create FiltersDto.cs

---
+++ backend/src/SneakersShop.Application/Catalog/DTOs/FiltersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SneakersShop.Application.Catalog.DTOs;
+
+public sealed record FiltersDto(
+    IReadOnlyList<string> Brands,
+    IReadOnlyList<string> Categories,
+    IReadOnlyList<string> Colors,
+    IReadOnlyList<decimal> Sizes,
+    PriceRangeDto PriceRange);
