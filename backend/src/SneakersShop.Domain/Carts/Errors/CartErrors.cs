2296338622e98e8a2883add245ece170c8191ad3
Vadym <minerking112256@gmail.com>
2026-08-22 14:47:26 +0200

    Create CartErrors.cs

---
+++ backend/src/SneakersShop.Domain/Carts/Errors/CartErrors.cs
@@ -0,0 +1,10 @@
+﻿using SneakersShop.Domain.Common.Results;
+
+namespace SneakersShop.Domain.Carts.Errors;
+
+public static class CartErrors
+{
+    public static Error CartNotFound =>
+        Error.NotFound("cart.notFound", "The cart for the specified user was not found.");
+
+}
