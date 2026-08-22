125d33a50ab5e018d66c1a1196701dc755d0571e
Vadym <minerking112256@gmail.com>
2026-08-22 14:47:20 +0200

    Create GetCartQuery.cs

---
+++ backend/src/SneakersShop.Application/Carts/Queries/GetCart/GetCartQuery.cs
@@ -0,0 +1,6 @@
+﻿using SneakersShop.Application.Abstractions.Queries;
+using SneakersShop.Application.Carts.DTOs;
+
+namespace SneakersShop.Application.Carts.Queries.GetCart;
+
+public record GetCartQuery() : IQuery<CartDto>;
