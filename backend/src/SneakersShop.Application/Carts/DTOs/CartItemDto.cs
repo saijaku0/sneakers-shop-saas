7fab3d0fb7c8c973caeada89183021f315f65e7c
Vadym <minerking112256@gmail.com>
2026-08-22 14:47:17 +0200

    Create CartItemDto.cs

---
+++ backend/src/SneakersShop.Application/Carts/DTOs/CartItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SneakersShop.Application.Carts.DTOs;
+
+public sealed record CartItemDto(
+    Guid WarehouseItemId,
+    Guid ProductId,
+    string Model,
+    string BrandName,
+    string Color,
+    decimal SizeCm,
+    string PreviewImageUrl,
+    decimal UnitPrice,
+    int Quantity,
+    int Available,
+    bool IsAvailable);
