8efa2bf36afd04b515988105f9958ac2344f827c
Vadym <minerking112256@gmail.com>
2026-08-24 13:59:59 +0200

    Create OrdersController.cs

---
+++ backend/src/SneakersShop.API/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using SneakersShop.API.Extensions;
+using SneakersShop.Application.Orders.Command.CreateOrder;
+
+namespace SneakersShop.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Authorize]
+public sealed class OrdersController(ISender sender) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder(
+        [FromBody] CreateOrderCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await sender.Send(command, cancellationToken);
+        return result.ToActionResult();
+    }
+}
