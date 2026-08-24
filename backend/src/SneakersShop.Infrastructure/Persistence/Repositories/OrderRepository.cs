c6366c044a846e8f410192ca3df0ecb320bec556
Vadym <minerking112256@gmail.com>
2026-08-24 14:00:21 +0200

    Create OrderRepository.cs

---
+++ backend/src/SneakersShop.Infrastructure/Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using SneakersShop.Application.Abstractions.Repositories;
+using SneakersShop.Domain.Orders;
+
+namespace SneakersShop.Infrastructure.Persistence.Repositories;
+
+public sealed class OrderRepository(AppDbContext context) : IOrderRepository
+{
+    public Task AddAsync(Order order, CancellationToken cancellation = default)
+    {
+        context.Orders.Add(order);
+        return Task.CompletedTask;
+    }
+}
