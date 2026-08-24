e5509fd23acb86809ba45d5acf5546bc03b93ef2
Vadym <minerking112256@gmail.com>
2026-08-24 14:00:04 +0200

    Update IWarehouseItemRepository.cs

--- backend/src/SneakersShop.Application/Abstractions/Repositories/IWarehouseItemRepository.cs
+++ backend/src/SneakersShop.Application/Abstractions/Repositories/IWarehouseItemRepository.cs
@@ -5,4 +5,5 @@ namespace SneakersShop.Application.Abstractions.Repositories;
 public interface IWarehouseItemRepository
 {
     Task<WarehouseItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<WarehouseItem?>> GetByIdsAsync(IReadOnlyList<Guid> ids, CancellationToken cancellationToken = default);
 }
