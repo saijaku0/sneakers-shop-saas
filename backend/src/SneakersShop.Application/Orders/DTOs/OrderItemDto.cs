21d68cdef96ee5c3bf8612d277c577f6a88a3909
Vadym <minerking112256@gmail.com>
2026-08-25 15:06:26 +0200

    Create OrderItemDto.cs

---
+++ backend/src/SneakersShop.Application/Orders/DTOs/OrderItemDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SneakersShop.Application.Orders.DTOs;
+
+public sealed record OrderItemDto(
+    Guid WarehouseItemId,
+    string Model,
+    string BrandName,
+    string Color,
+    decimal SizeCm,
+    string PreviewImageUrl,
+    decimal UnitPrice,
+    int Quantity,
+    decimal DiscountAmount,
+    decimal TotalPrice);
