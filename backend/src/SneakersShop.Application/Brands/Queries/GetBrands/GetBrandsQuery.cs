a3927eb12433df695b0bb70d3ee0a3e53c894757
Vadym <minerking112256@gmail.com>
2026-08-25 15:07:24 +0200

    Create GetBrandsQuery.cs

---
+++ backend/src/SneakersShop.Application/Brands/Queries/GetBrands/GetBrandsQuery.cs
@@ -0,0 +1,6 @@
+﻿using SneakersShop.Application.Abstractions.Queries;
+using SneakersShop.Application.Brands.DTOs;
+
+namespace SneakersShop.Application.Brands.Queries.GetBrands;
+
+public record GetBrandsQuery() : IQuery<IReadOnlyList<BrandDto>>;
