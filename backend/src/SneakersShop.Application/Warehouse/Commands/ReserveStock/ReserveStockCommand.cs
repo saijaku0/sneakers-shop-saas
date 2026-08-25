20db6821fbedd4ee1fbdecae48d3a331a1de6680
Vadym <minerking112256@gmail.com>
2026-08-25 15:05:56 +0200

    Delete ReserveStockCommand.cs

--- backend/src/SneakersShop.Application/Warehouse/Commands/ReserveStock/ReserveStockCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using SneakersShop.Application.Abstractions.Commands;
-
-namespace SneakersShop.Application.Warehouse.Commands.ReserveStock;
-
-public sealed record ReserveStockCommand(Guid WarehouseItemId, int Quantity) : ICommand;
