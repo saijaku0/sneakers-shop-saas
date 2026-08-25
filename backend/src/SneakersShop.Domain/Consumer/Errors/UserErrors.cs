f74ece5a2c05a0d07755c3018a9a8e2fbc0a492e
Vadym <minerking112256@gmail.com>
2026-08-25 15:05:52 +0200

    Update UserErrors.cs

--- backend/src/SneakersShop.Domain/Consumer/Errors/UserErrors.cs
+++ backend/src/SneakersShop.Domain/Consumer/Errors/UserErrors.cs
@@ -6,4 +6,6 @@ public static class UserErrors
 {
     public static Error UserIsUnauthorized =>
         Error.Unauthorized("user.unauthorized", "The user is not authorized in the system.");
+    public static Error UserNotFound =>
+        Error.NotFound("user.notFound", "The user is not found in the system");
 }
