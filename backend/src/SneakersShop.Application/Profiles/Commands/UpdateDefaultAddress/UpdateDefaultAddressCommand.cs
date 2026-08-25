f63ada3c65042b542cd23d87816111f0e163c271
Vadym <minerking112256@gmail.com>
2026-08-25 15:06:11 +0200

    Create UpdateDefaultAddressCommand.cs

---
+++ backend/src/SneakersShop.Application/Profiles/Commands/UpdateDefaultAddress/UpdateDefaultAddressCommand.cs
@@ -0,0 +1,6 @@
+﻿using SneakersShop.Application.Abstractions.Commands;
+using SneakersShop.Application.Profiles.DTOs;
+
+namespace SneakersShop.Application.Profiles.Commands.UpdateDefaultAddress;
+
+public sealed record UpdateDefaultAddressCommand(AddressDto Address) : ICommand;
