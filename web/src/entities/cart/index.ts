dc93e78f4d6b0ab51a2c250cd08380d0b66a435f
Vadym <minerking112256@gmail.com>
2026-08-25 10:06:18 +0200

    Update index.ts

--- web/src/entities/cart/index.ts
+++ web/src/entities/cart/index.ts
@@ -1,11 +1,41 @@
-export type { Cart, CartItem } from "./model/types";
-export { useCart } from "./model/use-cart";
+export type {
+  Cart,
+  CartItem,
+  LocalCartItem,
+  UpdateCartQuantityRequest,
+} from "./lib/types";
 export {
   selectItemCount,
   selectSubtotal,
   selectHasOutOfStock,
   selectCanCheckout,
+  selectCartItems,
+  selectCartSyncStatus,
 } from "./model/selectors";
 export { CartButton } from "./ui/cart-button";
 export { CartItem as CartItemRow } from "./ui/cart-item";
 export { CartSummary } from "./ui/cart-summary";
+
+export { cartReducer, clampCartQuantity } from "./model/cart-slice";
+export type { CartSyncStatus } from "./model/cart-slice";
+
+export {
+  addToCart,
+  changeQuantity,
+  removeFromCart,
+  clearCart,
+  clearLocalCart,
+  setCartFromServer,
+  setCartItemQuantity,
+  startCartSync,
+  finishCartSync,
+} from "./model/cart-slice";
+
+export {
+  useGetCartQuery,
+  useLazyGetCartQuery,
+  useAddToCartMutation,
+  useRemoveFromCartMutation,
+  useUpdateCartItemQuantityMutation,
+  useSyncCartMutation,
+} from "./api/cart-api";
