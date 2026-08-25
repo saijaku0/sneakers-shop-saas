2f95a1833a6aa975e551033d8ce3ca919098bd2a
Vadym <minerking112256@gmail.com>
2026-08-25 15:06:17 +0200

    Create GetOrdersQuery.cs

---
+++ backend/src/SneakersShop.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,6 @@
+﻿using SneakersShop.Application.Abstractions.Queries;
+using SneakersShop.Application.Orders.DTOs;
+
+namespace SneakersShop.Application.Orders.Queries.GetOrders;
+
+public sealed record GetOrdersQuery() : IQuery<IReadOnlyList<OrderSummaryDto>>;
