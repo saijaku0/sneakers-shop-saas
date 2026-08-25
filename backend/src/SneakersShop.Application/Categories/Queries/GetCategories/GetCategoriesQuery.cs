fdb0d5c198c3b491e79313f77bf6c15292635fe8
Vadym <minerking112256@gmail.com>
2026-08-25 15:07:15 +0200

    Create GetCategoriesQuery.cs

---
+++ backend/src/SneakersShop.Application/Categories/Queries/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,6 @@
+﻿using SneakersShop.Application.Abstractions.Queries;
+using SneakersShop.Application.Categories.DTOs;
+
+namespace SneakersShop.Application.Categories.Queries.GetCategories;
+
+public record GetCategoriesQuery() : IQuery<IReadOnlyList<CategoryDto>>;
