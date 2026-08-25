695378f492b334b6d7ff0aaec0631b50256294dd
Vadym <minerking112256@gmail.com>
2026-08-25 15:07:34 +0200

    Create ProfileController.cs

---
+++ backend/src/SneakersShop.API/Controllers/ProfileController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using SneakersShop.API.Extensions;
+using SneakersShop.Application.Profiles.Commands.UpdateDefaultAddress;
+using SneakersShop.Application.Profiles.Queries.GetDefaultAddress;
+
+namespace SneakersShop.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public sealed class ProfileController(ISender sender) : ControllerBase
+{
+    [HttpGet("address")]
+    public async Task<IActionResult> GetDefaultAddress(CancellationToken cancellationToken = default)
+    {
+        var query = new GetDefaultAddressQuery();
+        var result = await sender.Send(query, cancellationToken);
+        return result.ToActionResult();
+    }
+
+    [HttpPut("address")]
+    public async Task<IActionResult> UpdateDefaultAddress(
+        [FromBody] UpdateDefaultAddressCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await sender.Send(command, cancellationToken);
+        return result.ToActionResult();
+    }
+}
