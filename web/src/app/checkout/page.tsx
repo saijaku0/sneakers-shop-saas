c2f6597604f9e8af6ab50a19e9095ab1039426d6
Vadym <minerking112256@gmail.com>
2026-08-25 10:06:02 +0200

    Create page.tsx

---
+++ web/src/app/checkout/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/shared/ui";
+import { CheckoutHeader } from "@/widgets/checkout-header/ui/checkout-header";
+
+export default function CheckoutPage() {
+  return (
+    <>
+      <CheckoutHeader />
+      <Container>CheckoutPage</Container>
+    </>
+  );
+}
