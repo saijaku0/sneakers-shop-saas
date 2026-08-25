ec349cf101cca4e886350f11eb764ddc416a8c81
Vadym <minerking112256@gmail.com>
2026-08-25 15:06:09 +0200

    Create UpdateDefaultAddressValidator.cs

---
+++ backend/src/SneakersShop.Application/Profiles/Commands/UpdateDefaultAddress/UpdateDefaultAddressValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace SneakersShop.Application.Profiles.Commands.UpdateDefaultAddress;
+
+public sealed class UpdateDefaultAddressValidator
+    : AbstractValidator<UpdateDefaultAddressCommand>
+{
+    public UpdateDefaultAddressValidator()
+    {
+        RuleFor(x => x.Address.Country)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Address.State)
+            .MaximumLength(100);
+
+        RuleFor(x => x.Address.City)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Address.Street)
+            .NotEmpty()
+            .MaximumLength(200);
+
+        RuleFor(x => x.Address.HouseNumber)
+            .NotEmpty()
+            .MaximumLength(20);
+
+        RuleFor(x => x.Address.ZipCode)
+            .NotEmpty()
+            .MaximumLength(20);
+    }
+}
