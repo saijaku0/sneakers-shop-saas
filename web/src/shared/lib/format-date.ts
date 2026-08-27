7fb19d38cfa75811548c1a6aa6c0686d71166902
Vadym <minerking112256@gmail.com>
2026-08-27 10:59:16 +0200

    Create format-date.ts

---
+++ web/src/shared/lib/format-date.ts
@@ -0,0 +1,6 @@
+export function formatDateTime(value: string): string {
+  return new Intl.DateTimeFormat("en-US", {
+    dateStyle: "medium",
+    timeStyle: "short",
+  }).format(new Date(value));
+}
