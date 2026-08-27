d02997b51e1f0805af706b377529822a4670639a
Vadym <minerking112256@gmail.com>
2026-08-27 11:02:23 +0200

    Update index.ts

--- web/src/entities/session/index.ts
+++ web/src/entities/session/index.ts
@@ -1,6 +1,10 @@
 export {
   sessionReducer,
+  selectRefreshToken,
   selectToken,
+  selectIsSessionHydrated,
   clearToken,
   setToken,
+  hydrateSession,
 } from "./model/session-slice";
+export { SessionHydrator } from "./ui/session-hydrator";
