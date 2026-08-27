d7aa7870b0817926e9ee6b411565c6b78e08d369
Vadym <minerking112256@gmail.com>
2026-08-27 10:59:13 +0200

    Update index.ts

--- web/src/shared/lib/index.ts
+++ web/src/shared/lib/index.ts
@@ -2,3 +2,5 @@ export { ThemeProvider } from "./theme-provider";
 export { capitalize } from "./capitalize";
 export { cn } from "./utils";
 export { type DefaultShippingAddress, type CardData } from "./types";
+export { formatDateTime } from "./format-date";
+export { useIsMobile } from "./use-mobile";
